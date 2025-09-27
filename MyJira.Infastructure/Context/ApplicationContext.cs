17974604ad90db310e6002b1b971443b34441bb1
Abdusamad Juraev <juraevabdusamad12@gmail.com>
2025-09-27 12:48:48 +0500

    added postgres connection

---
+++ MyJira.Infastructure/Context/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyJira.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyJira.Infastructure.Context
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Project> Projects { get; set; }
+    }
+}
