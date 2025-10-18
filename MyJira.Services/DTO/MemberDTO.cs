3ec4c98b5029b90675b4e3fdb4fc6c01b0e0e924
Abdusamad Juraev <juraevabdusamad12@gmail.com>
2025-10-18 14:48:34 +0500

    added basic project member logic

---
+++ MyJira.Services/DTO/MemberDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyJira.Services.DTO
+{
+    public class MemberDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
---
+++ MyJira.Services/ViewModel/ProjectMemberViewModel.cs
@@ -0,0 +1,16 @@
+﻿using MyJira.Entity.Entities;
+using MyJira.Services.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyJira.Services.ViewModel
+{
+    public class ProjectMemberViewModel
+    {
+        public List<MemberDTO> MemberDTOs { get; set; } = new List<MemberDTO>();
+        public List<ProjectDTO> Projects { get; set; } = new List<ProjectDTO>();
+    }
+}
